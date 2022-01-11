e354b8640ad86ded44801c6ea0bfa7b3142b8fa9
Deeptesh Bhattacharya <deeptesh_b@persistent.com>
2022-01-11 15:52:18 +0530

    updated files.

--- vars/msg.groovy
+++ vars/msg.groovy
@@ -3,6 +3,14 @@ def call(){
         stage('example'){
             sh "echo this is my step"
         }
+
+        stage('envoy'){
+            sh "echo this is my step"
+        }
+
+        stage('deploy'){
+            sh "echo this is my step"
+        }
     }
      }
 
