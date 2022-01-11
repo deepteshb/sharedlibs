75cd017eb65345d3c4d7d96d215357652e8befd4
Deeptesh Bhattacharya <deeptesh_b@persistent.com>
2022-01-11 16:30:26 +0530

    updated files.

--- vars/msg.groovy
+++ vars/msg.groovy
@@ -1,3 +1,34 @@
+package vars.msg
+class msg {
+   private int StudentID;
+   private String StudentName;
+	
+   void setStudentID(int pID) {
+      StudentID = pID;
+   }
+	
+   void setStudentName(String pName) {
+      StudentName = pName;
+   }
+	
+   int getStudentID() {
+      return this.StudentID;
+   }
+	
+   String getStudentName() {
+      return this.StudentName;
+   }
+	
+   /* static void main(String[] args) {
+      Student st = new Student();
+      st.setStudentID(1);
+      st.setStudentName("Joe");
+		
+      println(st.getStudentID());
+      println(st.getStudentName());
+   }  */
+}
+
 def call(standardPipeline){
     node{
         stage('example'){
